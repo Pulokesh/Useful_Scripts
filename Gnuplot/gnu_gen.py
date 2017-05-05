3c5c69a00b87ca56459a98fc091ce65d18e7f0d8
Ekadashi <ekadashipradhan@gmail.com>
2017-05-05 01:29:18 -0400

    latest update

--- Gnuplot/gnu_gen.py
+++ Gnuplot/gnu_gen.py
@@ -1,3 +1,16 @@
+#*********************************************************************************
+#* Copyright (C) 2016 Ekadashi Pradhan
+#* 
+#* This file is distributed under the terms of the GNU General Public License
+#* as published by the Free Software Foundation, either version 2 of
+#* the License, or (at your option) any later version.
+#* See the file LICENSE in the root directory of this distribution
+#* or <http://www.gnu.org/licenses/>.
+#*
+#*********************************************************************************/
+###########################################
+
+
 #! /usr/bin/env python
 import os
 import math
